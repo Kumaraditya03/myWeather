f466749911d85403749c21365febf30f8617525e
Kumaraditya03 <40833579+Kumaraditya03@users.noreply.github.com>
2020-06-21 23:08:14 +0530

    Removed unwanted spaces for better readability

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ request(url,function(err,res,body){
     response.write("<h1>"+ 'Temperature: '+data.main['temp']+'<br>'+'</h1>');
     response.write("</div></body></html>");
     console.log(body);
-    // Sample to check revert
-
-
-
-
 
 response.end();
 });
